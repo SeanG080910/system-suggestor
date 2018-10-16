c6267f4341cd6919d90c6115da54089e5ef06705
Zak <zak.s.butcher@parivedasolutions.com>
2018-10-16 07:29:20 -0400

    Include settings and extension recommendations.  Add styled components to project.  Create basic components using arrow functions to demonstrate easier way of making simple components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button} from './button';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as Fonts from "./components/fonts";
 
 class App extends Component {
   state = {
-    title: '',
-    currentQuestion: 0,
-  }
+    title: "",
+    currentQuestion: 0
+  };
   updateHeading = this.updateHeading.bind(this);
-  
 
   updateHeading() {
-    this.setState({title: 'Let the quiz begin'});
+    this.setState({ title: "Let the quiz begin" });
   }
 
   goToNextQuestion() {
-    this.setState({currentQuestion: this.state.currentQuestion++})
+    this.setState({ currentQuestion: this.state.currentQuestion++ });
   }
 
   render() {
@@ -25,10 +24,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">
-            {this.state.title === '' ? 'Welcome to the Lutron System Suggestor Tool!' : this.state.title}
+            {this.state.title === ""
+              ? "Welcome to the Lutron System Suggestor Tool!"
+              : this.state.title}
           </h1>
         </header>
-        <Button bob={this.goToNextQuestion}>Click to begin!</Button>
+        <Fonts.Heading1>This is a Heading1</Fonts.Heading1>
+        <Fonts.Heading2>This is a Heading2</Fonts.Heading2>
+        <Fonts.Heading3>This is a Heading3</Fonts.Heading3>
+        <Fonts.Paragraph>This is a Paragraph</Fonts.Paragraph>
+        <Fonts.RedParagraph>This is a RedParagraph</Fonts.RedParagraph>
       </div>
     );
   }
--- src/components/fonts/index.jsx
+++ src/components/fonts/index.jsx
@@ -1,1 +1,15 @@
-// TODO - Make a font component
+import React from "react";
+import styled from "styled-components";
+
+/** Heading Fonts for use within the application */
+export const Heading1 = props => <h1 {...props} />;
+export const Heading2 = props => <h1 {...props} />;
+export const Heading3 = props => <h1 {...props} />;
+
+/** Paragraph Font for use within the application */
+export const Paragraph = props => <p {...props} />;
+
+/** Fancy new thing! */
+export const RedParagraph = styled(Paragraph)`
+  color: red;
+`;
